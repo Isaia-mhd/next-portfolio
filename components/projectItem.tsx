ec9cbab4783ab33c8a34cb9e7a3d1f5cac92a0ae
Isaia-mhd <mohamedesaie21@gmail.com>
2025-01-10 12:31:00 +0100

    improve and fetch the api from laravel for the contact section

--- components/projectItem.tsx
+++ components/projectItem.tsx
@@ -5,11 +5,12 @@ import InfiniteMovingCardsDemo from './projectslide'
 const ProjectItem = () => {
   return (
     <BackgroundBeamsWithCollision className="w-full max-w-[80%] mx-auto flex flex-col justify-center items-center gap-6">
-      <div className="flex flex-col justify-center items-center">
+      <div id='project' className="flex flex-col justify-center items-center">
         <p className="text-4xl font-bold text-purple-700">My Portfolio</p>
         <p className="text-slate-400 text-sm font-bold">
           &lt;&gt;All projects&lt;/&gt;
         </p>
+      <InfiniteMovingCardsDemo />
       </div>
 
       {/* <div className="w-full h-[400px] bg-blue-950 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center px-4 rounded-lg mb-12">
@@ -18,7 +19,6 @@ const ProjectItem = () => {
         <div className="bg-slate-300 w-full h-[80%] p-3 rounded-lg "></div>
       </div> */}
 
-      <InfiniteMovingCardsDemo />
     </BackgroundBeamsWithCollision>
   )
 }
