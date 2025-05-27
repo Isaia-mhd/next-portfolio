49213de7a8562c8ead9b63d8715a531c99ad47ec
Isaia-mhd <mohamedesaie21@gmail.com>
2025-05-27 20:54:48 +0300

    set up the projectItem

--- components/projectItem.tsx
+++ components/projectItem.tsx
@@ -1,41 +1,60 @@
 "use client";
 import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import axios from "axios";
+import { useEffect, useState } from "react";
 // import React, { useState } from "react";
 
 const ProjectItem = () => {
-  // const [isActive, setIsActive] = useState(false);
-  const projects = [
-    {
-      name: "Xinda",
-      description: "Xinda is a collaborated project",
-      slug: "Xinda is a Wood treatment company",
-      tech: "Next Js",
-      status: "Collaboration",
-      imgUrl: "url('/images/xinda.png')",
-      url: "https://xinda.mg/",
-      active: true
-    },
-    {
-      name: "Realtor Clone",
-      description: "Realtor Clone is my practice project while I'm learning React Js",
-      slug: "Bk is a...",
-      tech: "React Js / Firebase",
-      status: "Personal",
-      imgUrl: "url('/images/realtor.png')",
-      url: "https://isaia-realtor-clone.vercel.app/",
-      active: true
-    },
-    {
-      name: "E-Commerce",
-      description: "This project is in progress.",
-      slug: "E-comm is a...",
-      tech: "Blade / Laravel",
-      status: "Personal",
-      imgUrl: "url('/images/ecommerce.png')",
-      url: "#",
-      active: false
-    },
-  ];
+  const [projects, setProject] = useState([]);
+  // const projects = [
+  //   {
+  //     name: "Xinda",
+  //     description: "Xinda is a collaborated project",
+  //     slug: "Xinda is a Wood treatment company",
+  //     tech: "Next Js",
+  //     status: "Collaboration",
+  //     imgUrl: "url('/images/xinda.png')",
+  //     url: "https://xinda.mg/",
+  //     active: true
+  //   },
+  //   {
+  //     name: "Realtor Clone",
+  //     description: "Realtor Clone is my practice project while I'm learning React Js",
+  //     slug: "Bk is a...",
+  //     tech: "React Js / Firebase",
+  //     status: "Personal",
+  //     imgUrl: "url('/images/realtor.png')",
+  //     url: "https://isaia-realtor-clone.vercel.app/",
+  //     active: true
+  //   },
+  //   {
+  //     name: "E-Commerce",
+  //     description: "This project is in progress.",
+  //     slug: "E-comm is a...",
+  //     tech: "Blade / Laravel",
+  //     status: "Personal",
+  //     imgUrl: "url('/images/ecommerce.png')",
+  //     url: "#",
+  //     active: false
+  //   },
+  // ];
+
+  const getProjects = async() => {
+    try {
+    
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/projects`);
+      console.log(response);
+      setProject(response.data)
+      
+    } catch (error) {
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(() =>{
+    getProjects();
+  }, [])
 
   return (
     <BackgroundBeamsWithCollision className="w-full max-w-[80%] mx-auto flex flex-col justify-center items-center gap-6">
@@ -47,8 +66,7 @@ const ProjectItem = () => {
         </p>
         {/* <InfiniteMovingCardsDemo /> */}
       </div>
-
-      <div className="w-full  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center px-4 rounded-lg">
+      {!Array.isArray(projects) ? <div className="text-center pt-6 text-amber-300 font-semibold"><p>No projects available!</p></div>:  <div className="w-full  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center px-4 rounded-lg">
         {projects.map((project) => (
           <div className="border-2 w-full h-[400px] p-3 rounded-lg hover:-translate-y-2 transition duration-500 ease-in-out hover:cursor-pointer " key={project.name}>
             <div
@@ -71,7 +89,9 @@ const ProjectItem = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div>}
+
+      
     </BackgroundBeamsWithCollision>
   );
 };
